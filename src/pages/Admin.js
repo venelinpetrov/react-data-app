b8ee969596c4c1bcc0ffdf5dbee577e354bfb4d1
venelinpetrov <vpetrov33tu@gmail.com>
2019-08-05 22:28:42 +0300

    Implement grid with details on the same page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { Grid, GridColumn } from '../components/Grid';
+import axios from 'axios';
+
 
 const Admin = () => {
+    const initialState = {
+        data: [],
+        loading: true,
+        selectedItem: null
+    }
+
+    const [state, setState] = useState(initialState);
+
+    useEffect(() => {
+        const getOrders = async () => {
+
+            const { data } = await axios.get('http://northwind.servicestack.net/orders.json');
+            setState({
+                ...state,
+                data: data && data.Results && data.Results.map(item => item.Order),
+                loading: false
+            });
+        }
+
+        getOrders();
+    }, []);
+
+    function handleRowSelect({dataItem}) {
+        setState({
+            ...state,
+            selectedItem: dataItem
+        });
+    }
     return (
-        <div>
-            <h2>Admin</h2>
-            <p>This is the admin panel</p>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-xs-12">
+                    <h2>Admin</h2>
+                    <p>This is the admin panel</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <Grid data={state.data} loading={state.loading} onRowSelect={handleRowSelect}>
+                        <GridColumn field="Id" />
+                        <GridColumn field="CustomerId"/>
+                    </Grid>
+                </div>
+                <div className="col-md-6">
+                    {state.selectedItem ? (
+                        Object.keys(state.selectedItem).map((key, i) => <div key={i}>{state.selectedItem[key]}</div>)
+                    ) : (
+                        <div>Select item...</div>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
