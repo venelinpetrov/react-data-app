2796e887e2a46821d0e6e67c5f3e4c288df3ceab
Venelin Petrov <venelin.petrov@progress.com>
2019-08-05 13:08:02 +0300

    Navigation active item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import './AuthButton';
 import AuthButton from './AuthButton';
 
-function Header() {
+const Header = withRouter(({match, location}) => {
+    console.log(match, location)
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/admin" className="nav-link">Admin Panel</Link>
-                    </li>
-                    <li className="nav-item">
-                        <AuthButton className="nav-link"/>
-                    </li>
+                    <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+                    <NavLink to="/admin" className="nav-link" activeClassName="active">Admin Panel</NavLink>
+                    <AuthButton className="nav-link" activeClassName="active"/>
                 </div>
             </div>
         </nav>
     )
-}
+})
 
 export default Header;
